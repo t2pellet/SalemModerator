ac32a255a458fcd682ba1b548da215b11888a89c
Tenzin Pelletier <pelletiertenzin2000@gmail.com>
2022-10-17 21:36:40 +0200

    setup linting

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { registerRootComponent } from 'expo';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
+
+function App() {
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+        </View>
+    );
+}
+
+registerRootComponent(App);
