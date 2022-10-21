55c7ef5e74dcf16282a88b82b1d279617d91d20d
Tenzin Pelletier <pelletiertenzin2000@gmail.com>
2022-10-21 18:50:39 +0200

    night recordings

--- src/utils/sounds.ts
+++ src/utils/sounds.ts
@@ -3,6 +3,13 @@ const dawnWitchStart = require('../assets/sound/dawn/witch_start.mp3');
 const dawnWitchEnd = require('../assets/sound/dawn/witch_end.mp3');
 const dawnEnd = require('../assets/sound/dawn/dawn_end.mp3');
 
+const nightStart = require('../assets/sound/night/night_start.mp3');
+const nightWitchStart = require('../assets/sound/night/witch_start.mp3');
+const nightWitchEnd = require('../assets/sound/night/witch_end.mp3');
+const constableStart = require('../assets/sound/night/constable_start.mp3');
+const constableEnd = require('../assets/sound/night/constable_end.mp3');
+const nightEnd = require('../assets/sound/night/night_end.mp3');
+
 export const dawn = {
     start: dawnStart,
     witchStart: dawnWitchStart,
@@ -10,6 +17,16 @@ export const dawn = {
     end: dawnEnd
 };
 
+export const night = {
+    start: nightStart,
+    witchStart: nightWitchStart,
+    witchEnd: nightWitchEnd,
+    constableStart,
+    constableEnd,
+    end: nightEnd
+};
+
 export default {
-    dawn
+    dawn,
+    night
 };
