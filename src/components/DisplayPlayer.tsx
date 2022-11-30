28f0691caa7da4892412eb105e43504e0ac4fe49
Tenzin Pelletier <pelletiertenzin2000@gmail.com>
2022-11-30 09:44:33 +0100

    player display styling

--- src/components/DisplayPlayer.tsx
+++ src/components/DisplayPlayer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Text } from 'react-native-paper';
 
 interface DisplayProps {
     playerName: string;
@@ -9,9 +10,9 @@ export default function DisplayPlayer(props: DisplayProps) {
     const { playerName, description } = props;
 
     return (
-        <div className="displayPlayer">
-            <h1>{playerName}</h1>
-            <p>{description}</p>
+        <div className="displayPlayer" style={{ textAlign: 'center' }}>
+            <Text variant="displayMedium">{playerName}</Text>
+            <Text variant="bodyMedium">{description}</Text>
         </div>
     );
 }
